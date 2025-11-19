663faa060a6f600f49cf07e4633f3f7e187d8eda
abhinvshrma <abhisharma.dev0@gmail.com>
2025-11-19 23:17:05 +0530

    Created input output steams
    server now is ready to take input from clients and echo it back to them ( on terminal)

--- server.java
+++ server.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +17,20 @@ public class server {
             // Making a socket for client and Waiting for client to join
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected "+ clientSocket.getInetAddress().getHostAddress());
+
+            // Setting up input and ouput streams 
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            // Takes message from client and echoes back 
+            String inputLine;
+            while((inputLine = in.readLine()) != null){
+                System.out.println("Client says: "+ inputLine);
+                out.println("Server received: "+ inputLine);
+            }
+            in.close();
+            out.close();
+            clientSocket.close();
         }
         catch(IOException e){
             System.out.println("Couldn't listen on port "+ PORT);
